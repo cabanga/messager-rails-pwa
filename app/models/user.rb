ea0221b4b24be9b3db54a0160077df34fa4073fc
cabanga <manucabanga@gmail.com>
2019-12-10 15:52:14 +0100

    add model Message and contacts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :validatable
+  has_many :contacts
+  has_many :sents, class_name: "Message", foreign_key: "sender_id"
+  has_many :receiveds, class_name: "Message", foreign_key: "receiver_id"
 end
