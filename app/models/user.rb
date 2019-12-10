60b6ae1c948b3149e87d538f32454a458b67cd5c
cabanga <manucabanga@gmail.com>
2019-12-10 16:15:51 +0100

    config validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   has_many :contacts
   has_many :sents, class_name: "Message", foreign_key: "sender_id"
   has_many :receiveds, class_name: "Message", foreign_key: "receiver_id"
+
+  validates :name, presence: true
+
 end
