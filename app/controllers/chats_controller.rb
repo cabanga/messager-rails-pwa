53d09ee0bdc4d11788a53f1d4c18354580297454
cabanga <manucabanga@gmail.com>
2019-12-13 15:15:18 +0100

    list all conversations

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,9 @@
 class ChatsController < ApplicationController
 
     def index
-        
+        @users = User.joins(:sent).where(messages: { receiver_id: current_user })
+        @users += User.joins(:received).where(messages: { sender_id: current_user })
+        @users.uniq!
     end
 
     def show
