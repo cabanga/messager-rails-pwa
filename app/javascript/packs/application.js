2356b0106bbf36df72c643af314b3cce635182e6
cabanga <manucabanga@gmail.com>
2019-12-13 17:03:43 +0100

    configuration of PWA

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -24,3 +20,11 @@ document.addEventListener('turbolinks:load', function() {
   })
   
 })
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+  .then(function(reg) {
+    console.log('[Companion]', 'Service worker registered!');
+    console.log(reg);
+  });
+}
